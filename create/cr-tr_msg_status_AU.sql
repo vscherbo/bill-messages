77dec2c886f1a823315e2563fa340f80dae6f627
Vladimir Scherbo <vscherbo@gmail.com>
2015-04-28 16:37:09 +0300

    Добавлена обработка неудачных сообщений

--- create/cr-tr_msg_status_AU.sql
+++ create/cr-tr_msg_status_AU.sql
@@ -7,7 +7,7 @@ CREATE TRIGGER "trg_msg_status_AU"
   ON "СчетОчередьСообщений"
   FOR EACH ROW
   -- WHEN ((NEW.msg_status = 999) AND (NEW.msg_status <> OLD.msg_status))
-  WHEN ((NEW.msg_status >= 10) AND (NEW.msg_status <> OLD.msg_status))
+  WHEN ( ( (NEW.msg_status >= 10) OR (NEW.msg_status = 0) ) AND (NEW.msg_status <> OLD.msg_status))
   EXECUTE PROCEDURE "fntr_Сообщение_в_ДозвонНТУ"();
 -- COMMENT ON TRIGGER "trg_msg_status_AU" ON "СчетОчередьСообщений" IS 'При присвоении msg_status значения 999 (пришла квитанция о доставке) заносит сообщение в ДозвонНТУ';
 COMMENT ON TRIGGER "trg_msg_status_AU" ON "СчетОчередьСообщений" IS 'При присвоении msg_status значения >= 10 (сообщение об ошибке или пришла квитанция о доставке) заносит сообщение в ДозвонНТУ';
