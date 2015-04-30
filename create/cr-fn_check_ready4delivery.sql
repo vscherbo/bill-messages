6f8d11caee417c13b3e85d4f06c1caa02ceb3086
Vladimir Scherbo <vscherbo@gmail.com>
2015-04-30 13:45:14 +0300

    В текст письма о готовности добавлено 'Напомните клиенту ...'

--- create/cr-fn_check_ready4delivery.sql
+++ create/cr-fn_check_ready4delivery.sql
@@ -8,42 +8,40 @@ $BODY$DECLARE
     b RECORD;
     mstr varchar(255);
     loc_msg_to integer;
-    -- loc_msg_status integer := 998; -- deferred while debug period
-    loc_msg_status integer := 1;
-
 BEGIN
-FOR b IN SELECT "№ счета",
-                "Дата счета",
-                -- Хозяин, 
-                -- fn_bill_payment("№ счета") AS Paym, 
-                предок,
-                Сумма
-            FROM Счета
-            WHERE 
-                Готов = 't'
-                AND Отгружен = 'f'
-                AND Отменен = 'f'
-                AND Отгрузка = 'Самовывоз'
-                AND Накладная IS NULL
-                AND Фактура IS NULL
-                AND фирма <> 'АРКОМ'
-                AND "Дата счета" > '2014-06-01' 
-                AND Хозяин <> 91
-                AND Сумма = fn_bill_payment("№ счета")
+    FOR b IN SELECT "№ счета",
+                    "Дата счета",
+                    -- Хозяин, 
+                    -- fn_bill_payment("№ счета") AS Paym, 
+                    предок,
+                    Сумма
+                FROM Счета
+                WHERE 
+                    Готов = 't'
+                    AND Отгружен = 'f'
+                    AND Отменен = 'f'
+                    AND Отгрузка = 'Самовывоз'
+                    AND Накладная IS NULL
+                    AND Фактура IS NULL
+                    AND фирма <> 'АРКОМ'
+                    AND "Дата счета" > '2014-06-01' 
+                    AND Хозяин <> 91
+                    AND Сумма = fn_bill_payment("№ счета")
     LOOP
---       RAISE NOTICE '№ счета,=%', b."№ счета" ; 
+        -- RAISE NOTICE '№ счета,=%', b."№ счета" ; 
 
-       IF b.предок = b."№ счета" THEN
-          mstr := 'Заказ ' || to_char(b."№ счета", 'FM9999-9999');
-       ELSE 
-          mstr := 'Заказ ' || to_char(b.предок, 'FM9999-9999') || '/' || to_char(b."№ счета", 'FM9999-9999');
-       END IF;
-       mstr := mstr || ' от ' || to_char(b."Дата счета", 'YYYY-MM-DD');
+        mstr := 'Напомните, пожалуйста, клиенту, что ';
+        IF b.предок = b."№ счета" THEN
+            mstr := 'заказ ' || to_char(b."№ счета", 'FM9999-9999');
+        ELSE 
+            mstr := 'заказ ' || to_char(b.предок, 'FM9999-9999') || '/' || to_char(b."№ счета", 'FM9999-9999');
+        END IF;
+        mstr := mstr || ' от ' || to_char(b."Дата счета", 'YYYY-MM-DD');
 
-       mstr := mstr || ' скомплектован и полностью оплачен. Готов к самовывозу.';
-       -- loc_msg_to := 2; -- в файл
-       loc_msg_to := 1; -- менеджеру
-       INSERT INTO СчетОчередьСообщений ("№ счета", msg_status, msg_to, msg) values (b."№ счета", loc_msg_status, loc_msg_to, mstr);
+        mstr := mstr || ' скомплектован и полностью оплачен. Готов к самовывозу.';
+        -- loc_msg_to := 2; -- в файл
+        loc_msg_to := 1; -- менеджеру
+        INSERT INTO СчетОчередьСообщений ("№ счета", msg_status, msg_to, msg) values (b."№ счета", 1, loc_msg_to, mstr);
 
     END LOOP;
 
