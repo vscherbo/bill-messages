f7af6ab4aceae0c47d68debf9b47f175d06cd9ab
Vladimir Scherbo <vscherbo@gmail.com>
2017-03-29 15:19:10 +0300

    отладочное сообщение при входе, переписаны комментарии

--- create/cr-get_bill_send_to.sql
+++ create/cr-get_bill_send_to.sql
@@ -5,20 +5,22 @@ CREATE OR REPLACE FUNCTION get_bill_send_to(
 $BODY$DECLARE
     loc_to VARCHAR;
 BEGIN
+RAISE NOTICE 'get_bill_send_to a_emp_code=%, a_to_addr=%', a_emp_code, a_to_addr;
 
 SELECT const_value INTO loc_to
-FROM arc_constants WHERE const_name = 'autobill_msg_to';
+FROM arc_constants WHERE const_name = 'autobill_msg_to'; -- в режиме отладки содержит email сотрудников через запятую
 
-IF NOT FOUND THEN 
+IF NOT FOUND THEN -- ошибка конфигурации, на задана настройка. Оповещение IT 
     loc_to := 'it@kipspb.ru'; 
 ELSE
-    IF 'to_client' = loc_to THEN -- to_client is reserved word
+    IF 'to_client' = loc_to THEN -- стандартная настройка, to_client - зарезервированное слово
         IF a_emp_code IN (SELECT "КодРаботника" FROM emp_company WHERE "Код" IN
-                                                                       -- (250531,210282,210243,216453,233690,252638)
-                                                                       (-1)
-                         ) THEN -- short dealers list 
-            loc_to := a_to_addr; -- overwrite to_addr value FROM arc_constants
-        ELSE
+                                                                       (250531,210282,210243,216453,233690,252638,231769,241625)
+                                                                       -- (-1)
+                         ) THEN -- Работник связан с Кодом дилера-тестера
+            loc_to := a_to_addr;
+            RAISE NOTICE 'Shortlist КодРаботника=%, to_addr=%', a_emp_code, a_to_addr;
+        ELSE -- Если не дилер-тестер, то заменяем получателя
             loc_to := 'arutyn@kipspb.ru,vscherbo@kipspb.ru';
         END IF;
     END IF;
