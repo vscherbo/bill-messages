8d864a1eb626e5c250c6e01f6b198be1135ea878
Vladimir Scherbo <vscherbo@gmail.com>
2017-05-11 16:52:42 +0300

    добавлен DISTINCT для предотвращения множественных оповещений; Оплата1 проверяется на 0 и NULL

--- create/cr-expiring_bill_alarm.sql
+++ create/cr-expiring_bill_alarm.sql
@@ -6,13 +6,16 @@ CREATE OR REPLACE FUNCTION expiring_bill_alarm()
   RETURNS void AS
 $BODY$
 INSERT INTO СчетОчередьСообщений ( "№ счета", msg, msg_to, msg_type ) 
-    SELECT Счета."№ счета", '4-ый день ' AS msg, 1 AS msg_to, 11 
+    SELECT DISTINCT Счета."№ счета", 
+           format(E'Добрый день,\nуведомляем Вас, что завтра истекает срок оплаты по счёту %s от %s.\nДля продления резерва и срока оплаты свяжитесь с вашим менеджером.', to_char(Счета."№ счета", 'FM9999-9999'), to_char(Счета."Дата счета", 'YYYY-MM-DD')) AS msg, 
+           2 AS msg_to, 11 AS msg_type --debug msg_to=file
+-- production to_client           1 AS msg_to, 11 AS msg_type  
         FROM Резерв 
         INNER JOIN Счета ON Резерв.Счет = Счета."№ счета" 
         LEFT JOIN vwДилеры ON Счета.Код = vwДилеры.Код
         WHERE Счета."Дата счета" =(current_date-4)::timestamp without time zone 
             AND Резерв.КогдаСнял Is Null
-            AND Счета.Оплата1=0
+            AND (Счета.Оплата1=0 OR Счета.Оплата1 Is Null)
             AND vwДилеры.Код Is Null
 $BODY$
   LANGUAGE sql VOLATILE
