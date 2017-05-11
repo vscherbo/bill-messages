d45b48380e89b71aaf74b109045be82eb0cde4fb
Vladimir Scherbo <vscherbo@gmail.com>
2017-05-11 16:14:45 +0300

    исключена конфигурационная переменная autobill_msg_to

--- create/cr-get_bill_send_to.sql
+++ create/cr-get_bill_send_to.sql
@@ -7,30 +7,20 @@ $BODY$DECLARE
 BEGIN
 RAISE NOTICE 'get_bill_send_to a_emp_code=%, a_to_addr=%', a_emp_code, a_to_addr;
 
-SELECT const_value INTO loc_to
-FROM arc_constants WHERE const_name = 'autobill_msg_to'; -- в режиме отладки содержит email сотрудников через запятую
-
-IF NOT FOUND THEN -- ошибка конфигурации, на задана настройка. Оповещение IT 
-    loc_to := 'it@kipspb.ru'; 
-ELSE
-    IF 'to_client' = loc_to THEN -- стандартная настройка, to_client - зарезервированное слово
-        IF a_emp_code IN (SELECT "КодРаботника" FROM emp_company WHERE "Код" IN
-                                                                       (250531,210282,210243,216453,233690,252638,231769,241625,255596,215789,253932,233038
+IF a_emp_code IN (SELECT "КодРаботника" FROM emp_company WHERE "Код" IN
+(250531,210282,210243,216453,233690,252638,231769,241625,255596,215789,253932,233038
 ,229233 -- ИП Карпов, Шалаевский
 ,245922 -- Р-Компонент, Хапёрский
 ,252836 -- ТПП ВВЦА, Алексеева
 ,254664 -- Промэлектрика, Барабаш
 ,231963 -- КИП-Сервис, Хапёрский
 )
-                                                                       -- (-1)
-                         ) THEN -- Работник связан с Кодом дилера-тестера
-            loc_to := a_to_addr;
-            RAISE NOTICE 'Shortlist КодРаботника=%, to_addr=%', a_emp_code, a_to_addr;
-        ELSE -- ТОЛЬКО до запуска автосчетов: Если не дилер-тестер, то заменяем получателя
-            loc_to := 'arutyun@kipspb.ru';
-        END IF;
-    END IF;
-END IF; -- NOT FOUND
+) THEN -- Работник связан с Кодом дилера-тестера
+    loc_to := a_to_addr;
+    RAISE NOTICE 'Shortlist КодРаботника=%, to_addr=%', a_emp_code, a_to_addr;
+ELSE -- ТОЛЬКО до запуска автосчетов: Если не дилер-тестер, то заменяем получателя
+    loc_to := 'arutyun@kipspb.ru';
+END IF;
 
 RETURN loc_to;
             
