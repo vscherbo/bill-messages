9444c7442637e9349ee8a225100c4c31f40e1b89
Vladimir Scherbo <vscherbo@gmail.com>
2018-04-20 11:52:47 +0300

    удалены устаревшие комментарии

--- create/cr-tr_msg_status_AU.sql
+++ create/cr-tr_msg_status_AU.sql
@@ -6,8 +6,6 @@ CREATE TRIGGER "trg_msg_status_AU"
   AFTER UPDATE OF msg_status
   ON "СчетОчередьСообщений"
   FOR EACH ROW
-  -- WHEN ((NEW.msg_status = 999) AND (NEW.msg_status <> OLD.msg_status))
   WHEN ( (NEW.msg_status >= 10) AND (NEW.msg_status <> OLD.msg_status))
   EXECUTE PROCEDURE "fntr_Сообщение_в_ДозвонНТУ"();
--- COMMENT ON TRIGGER "trg_msg_status_AU" ON "СчетОчередьСообщений" IS 'При присвоении msg_status значения 999 (пришла квитанция о доставке) заносит сообщение в ДозвонНТУ';
 COMMENT ON TRIGGER "trg_msg_status_AU" ON "СчетОчередьСообщений" IS 'При присвоении msg_status значения >= 10 (сообщение об ошибке или пришла квитанция о доставке) заносит сообщение в ДозвонНТУ';
