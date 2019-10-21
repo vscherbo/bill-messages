7a9ab3728afec669d2b6c04e04e1f8538b857c8f
Vladimir Scherbo <vscherbo@gmail.com>
2019-10-21 12:33:04 +0300

    SSL support

--- create/cr-send_attachment.sql
+++ create/cr-send_attachment.sql
@@ -2,6 +2,7 @@
 
 -- DROP FUNCTION public.send_attachment(text, text, text, text, integer, text, text, text, text, text[]);
 
+-- CREATE OR REPLACE FUNCTION public.send_attachment_ssl(
 CREATE OR REPLACE FUNCTION public.send_attachment(
     IN _from text,
     IN _password text,
@@ -27,12 +28,15 @@ from email.MIMEText import MIMEText
 from email.MIMEMultipart import MIMEMultipart
 from email.header import Header
 from email.mime.application import MIMEApplication
+import email.utils
 from datetime import datetime
 import pytz
 import re
 import os
 
-re_queued = re.compile('^reply: retcode .* queued as ([0-9A-F]{10,11})$')
+re_queued = re.compile('^reply: retcode .* queued .*as ([-0-9a-zA-Z]+)')
+re_timestamp = re.compile('[0-9]{10}-(.*)')
+
 
 sender = _from
 receivers = receiver.split(",")
@@ -91,6 +95,7 @@ if attachment_files is not None:
         msg.attach(att_pdf)
         plpy.notice("attached")
 
+msg.add_header('Message-ID', email.utils.make_msgid())        
 msg.add_header('Content-Transfer-Encoding', '8bit')
 msg.add_header('Reply-To', replyto)
 msg.add_header('Errors-To', 'it@kipspb.ru')
@@ -119,9 +124,12 @@ if port == -1:
     feml.close()
 else:
     try:
-        smtpObj = smtplib.SMTP(smtp,port)
-        if smtp != 'mail.arc.world':
-            smtpObj.starttls()
+        if port == 25:
+            smtpObj = smtplib.SMTP(smtp,port)
+            if smtp != 'mail.arc.world':
+                smtpObj.starttls()
+        else:
+            smtpObj = smtplib.SMTP_SSL('{0}:{1}'.format(smtp,port))
             smtpObj.login(_from, _password)
 
         save_smtplib_stderr = smtplib.stderr
@@ -135,12 +143,26 @@ else:
         smtplib.stderr = save_smtplib_stderr
         result_string = result.getvalue()
         result.close()
-        #plpy.notice(result_string)
         lines = result_string.splitlines()
+
         for line in lines:
+            #plpy.notice('line='+line)
             qid = re_queued.match(line)
             if qid:
-                out_msg_qid = qid.group(1)
+                msg_qid = qid.group(1)
+                plpy.notice('===<<<<<<=== msg_qid=' + msg_qid)
+
+                ts_match = re_timestamp.match(msg_qid)
+                if ts_match:
+                    out_msg_qid = ts_match.group(1)
+                else:
+                    out_msg_qid = msg_qid
+                plpy.notice('===>>>>>>>=== out_msg_qid=' + out_msg_qid)
+
+        #for line in lines:
+        #    qid = re_queued.match(line)
+        #    if qid:
+        #        out_msg_qid = qid.group(1)
 
     except smtplib.SMTPServerDisconnected:
       rc = 11
@@ -160,7 +182,10 @@ else:
       rc = 14
     except smtplib.SMTPHeloError:
       rc = 16
-    finally:
+    except Exception, e:
+      rc = 99
+      plpy.notice('Unknown:' + str(e))
+    else:
       smtpObj.quit()
 
 if len(rcpt_refused) > 0:
