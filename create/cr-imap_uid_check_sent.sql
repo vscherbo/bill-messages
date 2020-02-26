f70c3013580f21e2a4ff754296454993aeb1bf8f
Vladimir Scherbo <vscherbo@gmail.com>
2020-02-26 11:41:08 +0300

    production

--- create/cr-imap_uid_check_sent.sql
+++ create/cr-imap_uid_check_sent.sql
@@ -14,22 +14,74 @@ import re
 import imaplib
 import email
 import email.message
-import plpy
+from plpy import notice as pl_notice
+from plpy import warning as pl_warning
+from plpy import execute as pl_execute
+from plpy import SPIError as pl_SPIError
+
+re_rcv_by=re.compile("by .* id ([-0-9a-zA-Z]+);")
+re_rcv_by_str=re.compile("^Received: by .* id ([-0-9a-zA-Z]+);")
+re_to=re.compile("To: (.*)")
+
+# DEBUG
+#def pl_execute(arg_sql):
+#    pass
+
+######################
+def db_write(a_imap_srv, a_msg_qid='empty', a_msg_to='empty', a_msg_diag='empty', a_msg_status='empty', a_msg_problem='empty'):
+    loc_result = ''
+    ins_sql = "INSERT INTO send_problem(mail_srv, msg_qid, msg_to, msg_diag) VALUES (E'%s', E'%s', E'%s', E'%s')" % (a_imap_srv, a_msg_qid, a_msg_to, str(a_msg_diag).replace("'", "\\'"))
+    pl_notice('ins_sql=%s' % ins_sql)
+    try:
+        pl_execute(ins_sql)
+    except pl_SPIError, e:
+        loc_result = "INSERT INTO send_problem error, SQLSTATE %s" % e.sqlstate
+    else:
+        upd_sql = """UPDATE "СчетОчередьСообщений" SET msg_status = %s, msg_problem = E'%s' WHERE msg_qid = '%s'""" % (a_msg_status, a_msg_problem.replace("'", "\\'"), a_msg_qid)
+        pl_notice('upd_sql=%s' % upd_sql)
+        try:
+            pl_execute(upd_sql)
+        except pl_SPIError, e:
+            loc_result = "UPDATE СчетОчередьСообщений error, SQLSTATE %s" % e.sqlstate
+    return loc_result
+
+######################
+def to_extract(a_part):
+    loc_to = ''
+    if a_part:
+        if a_part.has_key('to'):
+            loc_to = a_part['to']
+            pl_notice('>>> FOUND loc_to=%s' % loc_to)
+    return loc_to
+
+######################
+def qid_extract(a_part):
+    loc_qid = None
+    if a_part:
+        rcv_list = a_part.get_all('Received')
+        if rcv_list:
+            #pl_notice('rcv_list=%s' % rcv_list)
+            for rcv in rcv_list:
+                res=re_rcv_by.match(rcv)
+                if res:
+                    non_delivered_id = res.group(1)
+                    loc_qid = non_delivered_id
+                    pl_notice('>>> FOUND loc_qid=%s' % loc_qid)
+        else:
+            pass
+            #pl_notice('rcv_list is None')
+    return loc_qid
+
 
 ret_result = ''
 msg_status = None
 
-re_rcv_by=re.compile("by .* id ([-0-9a-zA-Z]+);")
-
 imap = imaplib.IMAP4_SSL(arg_imap_srv)
 imap.login(arg_login, arg_password)
-# imap.login('no-reply@tabloled.ru', 'Ya-m@il')
 
 
 imap.select(arg_folder)
-#imap.select('mailer-daemon')
 
-#result, search_data = imap.uid('search', None, '(FROM "mailer-daemon@yandex.ru")')
 search_result, search_data = imap.uid('search', None, '(FROM "%s")' % arg_sender)
 msg_uid_list = search_data[0].split()
 
@@ -37,65 +89,83 @@ for msg_uid in msg_uid_list:
     fetch_result, msg_data = imap.uid('fetch', msg_uid, '(RFC822)')
 
     if fetch_result == 'OK':
+        msg_status = None
+        msg_diag = None
+        msg_problem = None
+        msg_to = None
+        msg_qid = None
+        pl_notice('################################')
+
         # raw_email
         msg = email.message_from_string(msg_data[0][1])
-
-        if (msg.is_multipart() and len(msg.get_payload()) > 1 and 
+        #_structure(msg)  # DEBUG
+        payloads_cnt = len(msg.get_payload())
+        #for i_payload in range(payloads_cnt):
+        #    pl_notice('payload[%s]=%s' % (i_payload, type(msg.get_payload(i_payload))))
+        if (msg.is_multipart() and len(msg.get_payload()) > 1 and
             msg.get_payload(1).get_content_type() == 'message/delivery-status'):
             for part in msg.walk():
                 part_type = part.get_content_type()
                 part_diag = dict(part.items())
-                plpy.notice('part_diag=%s' % part_diag)
+                #pl_notice('part_type=%s' % part_type)
+                #pl_notice('part.keys()=%s' % part.keys())
+                if part.has_key('to'):
+                    msg_to = to_extract(part)
+                msg_qid = qid_extract(part) or msg_qid
+                #pl_notice('msg.walk extracted msg_to={0}, msg_qid={1}'.format(msg_to, msg_qid))
                 if part.has_key('diagnostic-code'):
                     msg_problem = part['diagnostic-code']
+                    #pl_notice('has_key diagnostic-code msg_problem=%s' % msg_problem)
                     msg_diag = part_diag
-                    if not msg_status and part.has_key('status'):
-                        # try int()
-                        msg_status = int(part['status'].replace('.', ''))
+                    if not msg_status:
+                        if part.has_key('status'):
+                            #pl_notice("(not msg_status)=%s and part['status']=%s" % (not msg_status, part['status']) )
+                            # try int()
+                            msg_status = int(part['status'].replace('.', ''))
+                        else:
+                            msg_status = 994
+                    pl_notice('msg_status=%s, msg_diag=%s' % (msg_status, msg_diag))
+                if part_type == 'text/rfc822-headers':
+                    msg2 = msg.get_payload(2)
+                    if msg2:
+                        #pl_notice('msg2={0}'.format(msg2))
+                        #pl_notice('msg2_type=%s' % msg2.__class__.__name__)
+                        #pl_notice('len(msg2)=%s' % len(msg2))
+                        if not msg2.is_multipart():
+                            #pl_notice('msg2.is_multipart is FALSE')
+                            p2 = msg2.get_payload()
+                            #pl_notice('len(p2)=%s' % len(p2))
+                            list2 = p2.split('\n')
+                            for el in list2:
+                                #pl_notice('el={0}'.format(el))
+                                res=re_rcv_by_str.match(el)
+                                if res:
+                                    msg_qid = res.group(1)
+                                    pl_notice('in str={0}'.format(el))
+                                    pl_notice('>>> FOUND msg_qid=%s' % msg_qid)
+                                res=re_to.match(el)
+                                if res:
+                                    msg_to = res.group(1)
+                                    pl_notice('>>> FOUND msg_to=%s' % msg_to)
+
+            if msg_diag:
+                db_res = db_write(arg_imap_srv, msg_qid, msg_to, msg_diag, msg_status, msg_problem)
+                if db_res == '':
+                    copy_result = imap.uid('COPY', msg_uid, 'mailer-daemon|done')
+                    if copy_result[0] == 'OK':
+                        # TODO try
+                        mov, data = imap.uid('STORE', msg_uid , '+FLAGS', '(\Deleted)')
+                        imap.expunge()
                     else:
-                        msg_status = 990
-                    plpy.notice('msg_status=%s' % msg_status)
-                if part_type == 'message/rfc822':
-                    for subpart in part.walk():
-                        if subpart.has_key('to'):
-                            plpy.notice('subpart.to=%s' % subpart['to'])
-                            msg_to = subpart['to']
-                        rcv_list = subpart.get_all('Received')
-                        if rcv_list:
-                            for rcv in rcv_list:
-                                res=re_rcv_by.match(rcv)
-                                if res and msg_diag:
-                                    non_delivered_id = res.group(1)
-                                    msg_qid = non_delivered_id
-                                    plpy.notice('msg_qid=%s' % msg_qid)
-                                    ins_sql = "INSERT INTO send_problem(mail_srv, msg_qid, msg_to, msg_diag) VALUES (E'%s', E'%s', E'%s', E'%s')" % (arg_imap_srv, msg_qid, msg_to, str(msg_diag).replace("'", "\\'"))
-                                    plpy.notice('ins_sql=%s' % ins_sql)
-                                    msg_diag = None
-                                    try:
-                                        plpy.execute(ins_sql)
-                                    except plpy.SPIError, e:
-                                        ret_result = "INSERT INTO send_problem error, SQLSTATE %s" % e.sqlstate
-                                    else:
-                                        upd_sql = """UPDATE "СчетОчередьСообщений" SET msg_status = %s, msg_problem = E'%s' WHERE msg_qid = '%s'""" % (msg_status, msg_problem.replace("'", "\\'"), msg_qid)
-                                        plpy.notice('upd_sql=%s' % upd_sql)
-                                        try:
-                                            plpy.execute(upd_sql)
-                                        except plpy.SPIError, e:
-                                            ret_result = "UPDATE СчетОчередьСообщений error, SQLSTATE %s" % e.sqlstate
-                                            plpy.warning(ret_result)
-                                        else:
-                                            copy_result = imap.uid('COPY', msg_uid, 'mailer-daemon|done')
-                                            if copy_result[0] == 'OK':
-                                                # TODO try
-                                                mov, data = imap.uid('STORE', msg_uid , '+FLAGS', '(\Deleted)')
-                                                imap.expunge()
-                                            else:
-                                                plpy.warning(copy_result)
-                                                ret_result = copy_result
+                        ret_result = copy_result
+                        pl_warning(ret_result)
+                else:
+                    ret_result = db_res
+                    pl_warning(ret_result)
+
     else:
-        plpy.notice('Error reading msg_uid=%s, result=%s' % (msg_uid, result))
-
-
+        ret_result = fetch_res
+        pl_warning('Error reading msg_uid=%s, result=%s' % (msg_uid, res_result))
 
 imap.close()
 imap.logout()
--- create/imap_uid_check_sent.py
+++ create/imap_uid_check_sent.py
@@ -1,97 +1,188 @@
 #!/usr/bin/env python
+# -*- encoding: utf8 -*-
 
-import os
 import re
 import imaplib
 import email
 import email.message
-import pdb
+from email.iterators import _structure
+from plpy_stub import notice as pl_notice
+from plpy_stub import warning as pl_warning
+from plpy_stub import execute as pl_execute
+from plpy_stub import SPIError as pl_SPIError
+
 
 re_rcv_by=re.compile("by .* id ([-0-9a-zA-Z]+);")
+re_rcv_by_str=re.compile("^Received: by .* id ([-0-9a-zA-Z]+);")
+re_to=re.compile("To: (.*)")
 
-imap = imaplib.IMAP4_SSL('imap.yandex.ru')
-imap.login('no-reply@tabloled.ru', 'Ya-m@il')
-#print('imap.capabilities=%s' % str(imap.capabilities))
+def db_write(a_imap_srv, a_msg_qid='empty', a_msg_to='empty', a_msg_diag='empty', a_msg_status='empty', a_msg_problem='empty'):
+    loc_result = ''
+    ins_sql = "INSERT INTO send_problem(mail_srv, msg_qid, msg_to, msg_diag) VALUES (E'%s', E'%s', E'%s', E'%s')" % (a_imap_srv, a_msg_qid, a_msg_to, str(a_msg_diag).replace("'", "\\'"))
+    pl_notice('ins_sql=%s' % ins_sql)
+    try:
+        pl_execute(ins_sql)
+    except pl_SPIError, e:
+        loc_result = "INSERT INTO send_problem error, SQLSTATE %s" % e.sqlstate
+    else:
+        upd_sql = """UPDATE "СчетОчередьСообщений" SET msg_status = %s, msg_problem = E'%s' WHERE msg_qid = '%s'""" % (a_msg_status, a_msg_problem.replace("'", "\\'"), a_msg_qid)
+        pl_notice('upd_sql=%s' % upd_sql)
+        try:
+            pl_execute(upd_sql)
+        except pl_SPIError, e:
+            loc_result = "UPDATE СчетОчередьСообщений error, SQLSTATE %s" % e.sqlstate
+            warning(ret_result)
+    return loc_result
 
-#debug_sent_ids = ['hxtvfIfnBn-77IKejPm']
-#debug_sent_ids = ['CbYhVzIdq2-YdUGoXYo']
-debug_sent_ids = ['hxtvfIfnBn-77IKejPm',
-        'CbYhVzIdq2-YdUGoXYo',
-        'wJOoGztpJj-QbWGdlIx']
+def to_extract(a_part):
+    loc_to = ''
+    if a_part:
+        if a_part.has_key('to'):
+            loc_to = a_part['to']
+            pl_notice('>>> FOUND loc_to=%s' % loc_to)
+    return loc_to
 
-
-#print(imap.list())
-
-
-imap.select('mailer-daemon')
-#imap.select('Sent')
-
-result, search_data = imap.uid('search', None, '(FROM "mailer-daemon@yandex.ru")')
-msg_uid_list = search_data[0].split()
-#pdb.set_trace()
-
-for msg_uid in msg_uid_list:
-    result, msg_data = imap.uid('fetch', msg_uid, '(RFC822)')
-
-    print('msg_uid=%s, result=%s' % (msg_uid, result))
-    #print('msg_uid=%s, msg_data[0][0]=%s' % (msg_uid, msg_data[0][0]))
-    #continue
-
-    raw_email = msg_data[0][1]
-    #print('raw_email=%s' % raw_email)
-    #break
-    #continue
-    msg = email.message_from_string(raw_email)
-
-    print 'msg.keys=%s' % msg.keys()
-    #print 'msg.is_multipart=%s' % msg.is_multipart()
-    if (msg.is_multipart() and len(msg.get_payload()) > 1 and 
-        msg.get_payload(1).get_content_type() == 'message/delivery-status'):
-        for part in msg.walk():
-            part_type = part.get_content_type()
-            if part.has_key('action'):
-                #print 'part.items=%s' % part.items()
-                print 'action=%s, diagn=%s' % (part['action'], part['diagnostic-code'])
-                #print 'action=%s part.items=%s' % part['action'], part.items()
-            #else:
-            #    print 'part.keys=%s' % part.keys()
-            #print 'part.get_content_type()=%s' % part_type
-            if part_type == 'message/rfc822':
-                for subpart in part.walk():
-                    rcv_list = subpart.get_all('Received')
-                    if rcv_list:
-                        for rcv in rcv_list:
-                            res=re_rcv_by.match(rcv)
-                            if res:
-                                non_delivered_id = res.group(1)
-                                print('msg_id=%s' % non_delivered_id)
-                                if non_delivered_id in debug_sent_ids:
-                                    print('FOUND!')
-                                    result = imap.uid('COPY', msg_uid, 'mailer-daemon|done')
-
-                                    if result[0] == 'OK':
-                                        print('COPY OK')
-                                        mov, data = imap.uid('STORE', msg_uid , '+FLAGS', '(\Deleted)')
-                                        imap.expunge()
-                                    else:
-                                        print('result[0]=%s' % result[0])
-                                        print('type(result)=%s' % type(result))
-                                        for i_res in result:
-                                            print('i_res=%s' % i_res)
-
-        #pdb.set_trace()
-        # email is DSN
-        # print(msg.get_payload(0).get_payload()) # human-readable section
-
-        """
-        for dsn in msg.get_payload(1).get_payload():
-            print('action: %s' % dsn['action']) # e.g., "failed", "delivered"
-            #print('dsn: %s' % dsn)
-
-        if len(msg.get_payload()) > 2:
-           print(msg.get_payload(2)) # original message
-        """
+def qid_extract(a_part):
+    loc_qid = None
+    if a_part:
+        rcv_list = a_part.get_all('Received')
+        if rcv_list:
+            #pl_notice('rcv_list=%s' % rcv_list)
+            for rcv in rcv_list:
+                res=re_rcv_by.match(rcv)
+                if res:
+                    non_delivered_id = res.group(1)
+                    loc_qid = non_delivered_id
+                    pl_notice('>>> FOUND loc_qid=%s' % loc_qid)
+        else:
+            pass
+            #pl_notice('rcv_list is None')
+    return loc_qid
 
 
-imap.close()
-imap.logout()
+
+def imap_uid_check_sent(
+arg_imap_srv,
+arg_login,
+arg_password,
+arg_folder,
+arg_sender
+):
+    ret_result = ''
+
+    imap = imaplib.IMAP4_SSL(arg_imap_srv)
+    imap.login(arg_login, arg_password)
+
+
+    imap.select(arg_folder)
+
+    search_result, search_data = imap.uid('search', None, '(FROM "%s")' % arg_sender)
+    msg_uid_list = search_data[0].split()
+
+    for msg_uid in msg_uid_list:
+        fetch_result, msg_data = imap.uid('fetch', msg_uid, '(RFC822)')
+
+        if fetch_result == 'OK':
+            msg_status = None
+            msg_diag = None
+            msg_problem = None
+            msg_to = None
+            msg_qid = None
+            pl_notice('################################')
+
+            # raw_email
+            msg = email.message_from_string(msg_data[0][1])
+            #_structure(msg)  # DEBUG
+            payloads_cnt = len(msg.get_payload())
+            #for i_payload in range(payloads_cnt):
+            #    pl_notice('payload[%s]=%s' % (i_payload, type(msg.get_payload(i_payload))))
+            if (msg.is_multipart() and len(msg.get_payload()) > 1 and 
+                msg.get_payload(1).get_content_type() == 'message/delivery-status'):
+                for part in msg.walk():
+                    part_type = part.get_content_type()
+                    part_diag = dict(part.items())
+                    #pl_notice('part_type=%s' % part_type)
+                    #pl_notice('part.keys()=%s' % part.keys())
+                    if part.has_key('to'):
+                        msg_to = to_extract(part)
+                    msg_qid = qid_extract(part) or msg_qid
+                    pl_notice('msg.walk extracted msg_to={0}, msg_qid={1}'.format(msg_to, msg_qid))
+                    if part.has_key('diagnostic-code'):
+                        msg_problem = part['diagnostic-code']
+                        #pl_notice('has_key diagnostic-code msg_problem=%s' % msg_problem)
+                        msg_diag = part_diag
+                        if not msg_status:
+                            if part.has_key('status'):
+                                #pl_notice("(not msg_status)=%s and part['status']=%s" % (not msg_status, part['status']) ) 
+                                # try int()
+                                msg_status = int(part['status'].replace('.', ''))
+                            else:
+                                msg_status = 994
+                        pl_notice('msg_status=%s, msg_diag=%s' % (msg_status, msg_diag))
+                    if part_type == 'text/rfc822-headers':
+                        msg2 = msg.get_payload(2)
+                        if msg2:
+                            #pl_notice('msg2={0}'.format(msg2))
+                            #pl_notice('msg2_type=%s' % msg2.__class__.__name__)
+                            #pl_notice('len(msg2)=%s' % len(msg2))
+                            if not msg2.is_multipart():
+                                #pl_notice('msg2.is_multipart is FALSE')
+                                p2 = msg2.get_payload()
+                                #pl_notice('len(p2)=%s' % len(p2))
+                                list2 = p2.split('\n')
+                                for el in list2:
+                                    #pl_notice('el={0}'.format(el))
+                                    res=re_rcv_by_str.match(el)
+                                    if res:
+                                        msg_qid = res.group(1)
+                                        pl_notice('in str={0}'.format(el))
+                                        pl_notice('>>> FOUND msg_qid=%s' % msg_qid)
+                                    res=re_to.match(el)
+                                    if res:
+                                        msg_to = res.group(1)
+                                        pl_notice('>>> FOUND msg_to=%s' % msg_to)
+
+                                        
+                if msg_diag:
+                    db_res = db_write(arg_imap_srv, msg_qid, msg_to, msg_diag, msg_status, msg_problem)
+                    if db_res == '':
+                        pass
+                        '''
+                        else:
+                            copy_result = imap.uid('COPY', msg_uid, 'mailer-daemon|done')
+                            if copy_result[0] == 'OK':
+                                # TODO try
+                                mov, data = imap.uid('STORE', msg_uid , '+FLAGS', '(\Deleted)')
+                                imap.expunge()
+                            else:
+                                pl_warning(copy_result)
+                                ret_result = copy_result
+                        '''
+                                                    
+        else:
+            pl_notice('Error reading msg_uid=%s, result=%s' % (msg_uid, result))
+
+
+
+    imap.close()
+    imap.logout()
+    return ret_result
+
+
+imap_srv = 'imap.yandex.ru'
+login = 'no-reply@kipspb.ru'
+password = 'Never-adm1n'
+folder = 'mailer-daemon'
+sender = 'mailer-daemon@yandex.ru'
+
+
+res = imap_uid_check_sent(
+imap_srv,
+login,
+password,
+folder,
+sender
+)
+
+print('res=%s' % res)
+
